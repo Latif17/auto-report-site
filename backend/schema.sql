a021c5194bd30e9018fa8f54e0f01d53b52af2e6
Latif Muhammad <latif17@icloud.com>
2026-06-26 18:30:38 +0100

    chore: add status column to incidents schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -11,12 +11,13 @@ CREATE TABLE system_stats (
 );
 INSERT INTO system_stats (id) VALUES (1);
 
-CREATE TABLE incidents (
-  id SERIAL PRIMARY KEY,
-  time_of_smell TEXT,
-  smell_type TEXT,
-  business_location TEXT,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS incidents (
+    id SERIAL PRIMARY KEY,
+    time_of_smell TEXT NOT NULL,
+    smell_type TEXT NOT NULL,
+    business_location TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'pending',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now())
 );
 
 CREATE TABLE opted_in_user_reports (
