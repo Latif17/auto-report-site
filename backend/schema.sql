cebe829a0810bb4200069b72e2b8135603c309ec
Latif Muhammad <latif17@icloud.com>
2026-06-26 16:37:30 +0100

    db: add incidents and opted_in_user_reports tables

--- backend/schema.sql
+++ backend/schema.sql
@@ -10,3 +10,18 @@ CREATE TABLE system_stats (
   last_report_time timestamp with time zone
 );
 INSERT INTO system_stats (id) VALUES (1);
+
+CREATE TABLE incidents (
+  id SERIAL PRIMARY KEY,
+  time_of_smell TEXT,
+  smell_type TEXT,
+  business_location TEXT,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE opted_in_user_reports (
+  id SERIAL PRIMARY KEY,
+  incident_id INTEGER REFERENCES incidents(id),
+  user_email TEXT,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
